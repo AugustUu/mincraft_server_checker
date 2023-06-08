27e6b2dbf8a56c85a4956bda3b25b762ff857b46
auusitalo8059 <a@a.gov>
2023-06-08 14:00:39 -0700

    perforemance ++

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,11 @@ use futures::StreamExt;
 
 async fn check(hostname:&str) -> Option<(Response,&str)>{
 
-    if let Ok(mut stream) = TcpStream::connect((hostname, 25565)).await{
+    if let Ok(Ok(mut stream)) = timeout(Duration::from_secs(1), TcpStream::connect((hostname, 25565))).await{
 
-        match ping(&mut stream, hostname, 25565).await{
-            Ok(result) => {
-                return Some((result,hostname))
-            },
+        match timeout(Duration::from_secs(1), ping(&mut stream, hostname, 25565)).await{
+            Ok(Ok(result)) => Some((result,hostname)),
+            Ok(Err(_)) => None,
             Err(_) => None,
         }
     }else{
@@ -20,40 +19,50 @@ async fn check(hostname:&str) -> Option<(Response,&str)>{
     }
 }
 
+const PLAYER_FINDER: bool = false;
+
 #[tokio::main]
 async fn main() {
     let mut servers:Vec<String> = Vec::new();
     let timer = SystemTime::now();
     for x in 0..256 {
         for y in 0..256 {
-            for z in 0..8 {
-                let ip = format!("45.{}.{}.{}",x,y,z);
+            //for z in 57..60 {
+                let ip = format!("45.59.{}.{}",x,y);
                 servers.push(ip);
-            }
+            //}
         }
     }
     println!("Pinging: {} ips",servers.len());
 
     
-    let mut tasks = FuturesUnordered::<Pin<Box<dyn Future<Output = Result<Option<(Response,&str)>,Elapsed> >>>>::new();
+    let mut tasks = FuturesUnordered::<Pin<Box<dyn Future<Output = Option<(Response,&str)>>>>>::new();
     let mut itr = servers.iter();
 
-    for _ in 0..4600{
+    for _ in 0..40000{
         if let Some(ip) = itr.next() {
-            tasks.push(Box::pin(timeout(Duration::from_secs(1),check(ip))));
+            tasks.push(Box::pin(check(ip)));
         } else {    
             break;
         }
     }
 
-    while let Some(result) = tasks.next().await as Option<Result<Option<(Response,&str)>, Elapsed>>{
+    while let Some(result) = tasks.next().await as Option<Option<(Response,&str)>>{
 
         if let Some(ip) = itr.next() {
-            tasks.push(Box::pin(timeout(Duration::from_secs(1),check(ip))));
+            tasks.push(Box::pin(check(ip)));
         }
 
-        if let Ok(Some((result,ip))) = result{
-            println!("IP: {} Version: {} Players: {} Discription: {}",ip,result.version,result.online_players,result.description.text);
+        if let Some((result,ip)) = result as Option<(Response,&str)>{
+            if PLAYER_FINDER{
+                if let Some(players) = result.sample{
+                    for player in players.iter(){
+                        println!("{}            {}",player.name,result.description.text)
+                    }
+                }
+            }else{
+                println!("IP: {} Version: {} Players: {} Discription: {}",ip,result.version,result.online_players,result.description.text);
+            }
         }
 
     }
@@ -64,5 +73,8 @@ async fn main() {
 // 33
 // 17
 // 16
+// 10
 
-// 15 woo
+
+// 15 woo (releasse)
+// 9.5
